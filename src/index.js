8766a1eb1efc97c6e544123c1500fba4e09baef2
Varenik575 <invisibleredhead5@gmail.com>
2023-10-26 21:26:44 +0300

    rrr

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { getImages } from "./js/pixabay-api";
+import { fetchPhotosByQuery } from "./js/pixabay-api";
+const formInput = document.querySelector(".input");
+const form = document.querySelector('#search-form')
 
-getImages("dog");
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    fetchPhotosByQuery(formInput.value);
+}
+
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
+export async function fetchPhotosByQuery(query) {
+    return await axios.get(`https://pixabay.com/api/?`, {
+        params: {
+            image_type: 'photo',
+            orientation: 'horizontal',
+            q: query,
+            page: 1,
+            per_page: 40,
+            key: "40166916-9d1461e2ce0772333088e6da8",
+            safesearch: true,
+        }
+    })
+};
 
-axios.defaults.headers.common["x-api-key"] = "40166916-9d1461e2ce0772333088e6da8";
-axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export async function getImages(input) {
-    await axios.get(`/&key=40166916-9d1461e2ce0772333088e6da8&q=${input}&orientation=horizontal&image_type=photo&safesearch=true`);
-}
